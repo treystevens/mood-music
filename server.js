4fcf0637ee1873f050579514c9d8739ad7aa8a03
treykris <trey10027@gmail.com>
2018-08-12 23:54:58 -0400

    added dotenv, fixed for localhost use

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Mood = require('./models/mood');
+require('dotenv').config();
 const app = express();
+
+
 const redirect_uri = process.env.REDIRECT_URI || 'http://localhost:5000/user/callback/';
 
+const mongoDB = process.env.MONGODB_URI || 'mongodb://localhost/moodmusic';
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(mongoDB);
 
 mongoose.connection.once('open', (mssg) =>{
     console.log(`Connected to mongoDB`);
 });
 
-
+// Show login page when user types in moodmusic.com
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/login.html');
 });
@@ -34,15 +39,13 @@ app.get('/user', (req, res) => {
 });
 
 
+// User enters a mood
 app.get('/user/mood/:mood', (req,res) => {
   
-  let word = req.params.mood;
-  console.log(word);
-  console.log(req.params, `params`);
+  const word = req.params.mood;
 
   Mood.find({synonyms: word})
   .then((results) => {
-      console.log(results, `these are the results`);
       res.send(results);
   })
   .catch((err) => {
@@ -50,6 +53,7 @@ app.get('/user/mood/:mood', (req,res) => {
   });
 });
 
+// Authorize account
 app.get('/spotifylogin', function(req, res) {
   
   res.redirect('https://accounts.spotify.com/authorize?' +
@@ -89,7 +93,7 @@ app.get('/user/callback', function(req, res) {
 
 
 
-let port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 console.log(`Listening on port ${port}. Go to "/" or "/login" to initiate authentication flow.`);
 app.listen(port);
 
