fb360ed329104e750d736f63a8ad33d440b8733b
treykris <trey10027@gmail.com>
2019-07-05 19:38:22 -0400

    updated endpoint

--- src/api/getPlaylistTracks.js
+++ src/api/getPlaylistTracks.js
@@ -1,12 +1,6 @@
 import axios from './index';
-import { id } from '../user';
-
-// https://api.spotify.com/v1/playlists/{playlist_id}/tracks
 
 const getPlaylistTracks = playlistID =>
-  axios.get(`users/${id}/playlists/${playlistID}/tracks`);
+  axios.get(`playlists/${playlistID}/tracks`);
 
 export default getPlaylistTracks;
-
-// !!! TODO - Below is deprecated
-// users/${id}/playlists/${playlistID}/tracks
